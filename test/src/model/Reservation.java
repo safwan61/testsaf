a11c815f98b3f52100adb1ad856e0cc213f9738d
ISLAHI PC <ISLAHI PC@ISLAHI>
2018-05-02 13:37:54 +0100

    by

---
+++ test/src/model/Reservation.java
@@ -0,0 +1,42 @@
+package model;
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Reservation {
+
+    /**
+     * Default constructor
+     */
+    public Reservation() {
+    }
+
+    /**
+     * 
+     */
+    private String date;
+
+    /**
+     * 
+     */
+    private int numero;
+
+
+
+    /**
+     * 
+     */
+    public void annuler() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void confirmer() {
+        // TODO implement here
+    }
+
+}
